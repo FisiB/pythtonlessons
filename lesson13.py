c05d907c003a21499b6e4a27d7cd9473735dc5f3
FisiB <fisi.budakova@gmail.com>
2025-06-03 20:33:13 +0200

    Update lesson13.py

--- lesson13.py
+++ lesson13.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 array_2d= np.array([[1,2,3,4,5],[6,7,8,9,10]])
 
@@ -21,3 +22,29 @@ print(sum_coloum)
 
 sum_row=np.sum(array_2d, axis=1)
 print(sum_row)
+
+produce=["apples","grapes","bananas","lemons"]
+prices=[100,200,180,60]
+
+sale_produce=pd.Series(prices,index=produce)
+print(sale_produce)
+
+print(sale_produce["grapes"])
+
+total_sales=sale_produce.sum()
+print(total_sales)
+
+best_seller=sale_produce.idxmax()
+print(f"Best seller is:{best_seller}")
+
+data={
+    "Name" :["leon","dion","edin"],
+    "Age":[16,18,17],
+    "City":["Prishtina","Prizren","Gjakova"]
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+df=pd.read_csv(cs.csv)
+df.to_csv("dion", index= False)
