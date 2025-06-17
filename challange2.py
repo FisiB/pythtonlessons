84269ed1f810da5600e93b4ba67a10344467e003
FisiB <fisi.budakova@gmail.com>
2025-06-17 20:42:36 +0200

    Update challange2.py

--- challange2.py
+++ challange2.py
@@ -13,17 +13,23 @@ temp_avg = np.mean(temperature_column)
 
 print(f"Average temperature: {temp_avg:.2f}")
 
-dt['day'] = pd.to_datetime(dt['day'], errors='coerce')
+import pandas as pd
 
-dt['month'] = dt['day'].dt.month
+
+dt = pd.read_csv("weather_tokyo_data.csv", names=['year', 'day', 'temperature', 'humidity', 'atmospheric_pressure'], header=None)
+
+
+dt['date'] = pd.to_datetime(dt['year'].astype(str) + '-' + dt['day'], format='%Y-%m/%d', errors='coerce')
+
 
 dt['temperature'] = pd.to_numeric(dt['temperature'], errors='coerce')
 
-df = dt.dropna(subset=['temperature', 'month'])
+dt = dt.dropna(subset=['date', 'temperature'])
 
-monthly_avg = dt.groupby('month')['temperature'].mean()
+dt['month'] = dt['date'].dt.month
+dt['year_month'] = dt['date'].dt.to_period('M')
 
-monthly_avg.index = monthly_avg.index.map(lambda x: pd.to_datetime(str(x), format='%m').strftime('%B'))
+monthly_avg = dt.groupby('year_month')[['temperature']].mean()
 
-print("Monthly average temperatures:")
+print("Monthly average values:")
 print(monthly_avg)
