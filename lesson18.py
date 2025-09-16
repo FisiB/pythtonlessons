026dd8c8a3c8b9683fbb6aea062b5b72a221f671
FisiB <fisi.budakova@gmail.com>
2025-09-16 20:44:16 +0200

    sdiii

---
+++ lesson18.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# df=pd.DataFrame({
+#     "Name":["Dion","Darsej","Jon"],
+#     "Age":[17,19,21],
+#     "City":["Prishtina","Prizren","Gjakova"]
+# })
+
+# st.write(df)
+
+books_df=pd.read_csv("file.csv")
+st.title("Best selling books")
+st.write("The best selling books from 2009-2022")
+st.subheader("The summery of statistics")
+totalbooks=books_df.shape[0]
+unique_titels=books_df["Name"].nunique()
+avarage_rating=books_df["User Rating"].mean()
+avarage_price=books_df["Price"].mean()
+
+col1,col2,col3,col4=st.columns(4)
+col1.metric("Total books",totalbooks)
+col2.metric("Unqiue names",unique_titels)
+col3.metric("Ratings",avarage_rating)
+col4.metric("Price",avarage_price)
