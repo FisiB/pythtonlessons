0b18453c0e518e1de5db62ab695f86fc18e0a98b
FisiB <fisi.budakova@gmail.com>
2025-10-16 19:45:23 +0200

    Update lesson21.py

--- lesson21.py
+++ lesson21.py
@@ -2,19 +2,16 @@
 #     full_name = first_name.title() + " " + last_name.title()
 #     return full_name
 # print(format_name("Kung", "Lao"))
+# def get_full_name(first_name: str, last_name: str):
+#     full_name = first_name.title() + " " + last_name.title()
+#     return full_name
+
+# print(get_full_name("Leo", "Nardo"))
+
+# def get_item(item_a: str, item_b: int, item_c: float, item_d: bytes, item_e: bool):
+#     return item_a,item_b,item_c,item_d,item_e
+# print(get_item("str", "2", "3", "4,5", True ))
 from fastapi import FastAPI
-
-def get_full_name(first_name: str, last_name: str):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
-
-print(get_full_name("Leo", "Nardo"))
-
-def get_item(item_a: str, item_b: int, item_c: float, item_d: bytes, item_e: bool):
-    return item_a,item_b,item_c,item_d,item_e
-print(get_item("str", "2", "3", "4,5", True ))
-
-
 
 app = FastAPI()
 
